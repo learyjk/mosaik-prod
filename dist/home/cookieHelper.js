09e18d4771d6451fc899f146e2da34b79051a2d2
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-03-03 09:55:04 -0800

    1.0.4

--- dist/home/cookieHelper.js
+++ dist/home/cookieHelper.js
@@ -1,2 +1,2 @@
-(()=>{function r(e,t,n){let o=new Date;o.setTime(o.getTime()+n*24*60*60*1e3);let i="expires="+o.toUTCString();document.cookie=e+"="+t+";"+i+";path=/"}function s(e){return document.cookie.match("(^|;)\\s*"+e+"\\s*=\\s*([^;]+)")?.pop()||null}function c(e){document.cookie=e+"=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;"}})();
+(()=>{function r(e,t,n){let o=new Date;o.setTime(o.getTime()+n);let i="expires="+o.toUTCString();document.cookie=e+"="+t+";"+i+";path=/"}function s(e){return document.cookie.match("(^|;)\\s*"+e+"\\s*=\\s*([^;]+)")?.pop()||null}function c(e){document.cookie=e+"=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;"}})();
 //# sourceMappingURL=cookieHelper.js.map
